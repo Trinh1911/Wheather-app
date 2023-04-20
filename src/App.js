283fa89b1cd6ede19ac1f7140ca9cf76c9d35307
Trinh1911 <nguyenkimtrinhntn@gmail.com>
2023-04-20 15:58:51 +0700

    reponsive

--- src/App.js
+++ src/App.js
@@ -30,28 +30,33 @@ function App() {
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>{data.name}</p>
+            <h2>{data.name}</h2>
           </div>
           <div className="temp">
             {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
           </div>
-          <div className="description">
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
-          </div>
         </div>
-
         {data.name !== undefined &&
           <div className="bottom">
             <div className="feels">
-              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              <div className='temperature'>
+                <div className='img-clouds'></div>
+                {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              </div>
               <p>Feels Like</p>
             </div>
             <div className="humidity">
+            <div className='temperature'>
+              <div className='img-humidity'></div>
               {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+            </div>
               <p>Humidity</p>
             </div>
             <div className="wind">
+            <div className='temperature'>
+              <div className='img-wind'></div>
               {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+            </div>
               <p>Wind Speed</p>
             </div>
           </div>
