7df11c5eb80687c844162a0daec5dadf5636005b
Trinh1911 <nguyenkimtrinhntn@gmail.com>
2023-04-30 23:06:17 +0700

    complete

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import axios from 'axios'
 function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
-
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=f5d8af55b91d0cae848fc3ddaf9e9072`
 
   const searchLocation = (event) => {
@@ -17,6 +16,22 @@ function App() {
     }
   }
 
+  let contentLoacation;
+
+  if (data.weather && data.weather[0].description) 
+  {
+    if (data.weather[0].description === 'light rain') {
+      contentLoacation = <div className='img-location-rain'></div>
+    }
+    else if (data.weather[0].description === 'clear sky') {
+      contentLoacation = <div className='img-sky-clear'></div>
+    }
+    else {
+      contentLoacation = <div className='img-location-drizzle'></div>
+    }
+  }
+  
+  
   return (
     <div className="app">
       <div className="search">
@@ -63,7 +78,7 @@ function App() {
         }
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
